25a901a6c1e2c3501bc3a718baa0c8edbeaef88d
Travis-Owens <30815131+Travis-Owens@users.noreply.github.com>
2020-10-15 14:47:56 -0400

    Discord Cog for YouTube Subscription Management
    Created a cog for the discord_async interface.
    
    Command:
    `k!youtube <add/del/delete> <YouTube Channel URL>`
    
    This cog will make a request with the Discord channel ID and the YouTube Channel URL to the corresponding API route.

---
+++ discord_async/cogs/youtube.py
@@ -0,0 +1,48 @@
+import discord
+from discord.ext import commands
+
+import requests
+import json
+import os
+
+usage = '<action> <YouTube channel URL>'
+usage_p = os.getenv('COMMAND_PREFIX') + ' ' + usage
+
+class twitch_cog(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(usage=usage)
+    async def youtube(self, ctx, *args):
+
+        actions = ['add', 'del', 'delete']
+
+        if(len(args) in [0,1]):
+            await ctx.send('Incorrect Arguments! \n ' + usage_p)
+            return
+
+        if args[0] not in actions:
+            await ctx.send('Allowed Actions: ' + str(actions))
+            return
+
+        action              = str(args[0])
+        youtube_channel_url = str(args[1])
+        discord_channel_id  = str(ctx.channel.id)
+
+        headers = {'auth':os.getenv('API_AUTH_CODE').strip("\r"), 'youtube-channel-url':youtube_channel_url, 'discord-channel-id': discord_channel_id}
+
+        if(action == 'add'):
+            resp = requests.get(str(os.getenv('API_URL').strip("\r") + "/youtube/manage/add"), headers=headers)
+        elif(action in ['del', 'delete']):
+            resp = requests.get(str(os.getenv('API_URL').strip("\r") + "/youtube/manage/delete"), headers=headers)
+
+        if(resp.status_code == 200):
+            await ctx.send(resp.text)
+        else:
+            print(resp.status_code)
+            await ctx.send('Back-end server error!')
+
+
+def setup(bot):
+    bot.add_cog(twitch_cog(bot))
