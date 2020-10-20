81faa1b6ca90c2e38db6386f09f4dd94a3cbc5e7
Travis-Owens <30815131+Travis-Owens@users.noreply.github.com>
2020-10-20 15:02:30 -0400

    Created Schema for MySQL Database
    Created a schema for the entire database along with the individual tables.

--- database/keshbotics.sql
+++ database/keshbotics.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2.1
--- http://www.phpmyadmin.net
+-- version 4.6.6deb5
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Sep 21, 2020 at 04:03 PM
--- Server version: 5.7.31-0ubuntu0.16.04.1
--- PHP Version: 7.2.33-1+ubuntu16.04.1+deb.sury.org+1
+-- Host: localhost:3306
+-- Generation Time: Oct 20, 2020 at 03:01 PM
+-- Server version: 5.7.30-0ubuntu0.18.04.1
+-- PHP Version: 7.2.24-0ubuntu0.18.04.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -43,6 +43,13 @@ CREATE TABLE `settings` (
   `setting_value` text COLLATE utf8_bin
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+--
+-- Dumping data for table `settings`
+--
+
+INSERT INTO `settings` (`setting_id`, `setting_key`, `setting_value`) VALUES
+(1, 'oauth_token', '');
+
 -- --------------------------------------------------------
 
 --
@@ -70,15 +77,32 @@ CREATE TABLE `twitch` (
   `streaming` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `youtube`
+--
+
+CREATE TABLE `youtube` (
+  `id` int(11) NOT NULL,
+  `yt_channel_id` text COLLATE utf8_bin NOT NULL,
+  `disc_channel_id` text COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `youtube_submissions`
+--
+
+CREATE TABLE `youtube_submissions` (
+  `id` int(11) NOT NULL,
+  `yt_video_id` text COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `cron_event`
---
-ALTER TABLE `cron_event`
-  ADD PRIMARY KEY (`event_id`);
 
 --
 -- Indexes for table `settings`
@@ -98,6 +122,47 @@ ALTER TABLE `stream_metrics_time`
 ALTER TABLE `twitch`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `youtube`
+--
+ALTER TABLE `youtube`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `youtube_submissions`
+--
+ALTER TABLE `youtube_submissions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `settings`
+--
+ALTER TABLE `settings`
+  MODIFY `setting_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `stream_metrics_time`
+--
+ALTER TABLE `stream_metrics_time`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+--
+-- AUTO_INCREMENT for table `twitch`
+--
+ALTER TABLE `twitch`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+--
+-- AUTO_INCREMENT for table `youtube`
+--
+ALTER TABLE `youtube`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+--
+-- AUTO_INCREMENT for table `youtube_submissions`
+--
+ALTER TABLE `youtube_submissions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ database/tables/cron_event.sql
@@ -0,0 +1,36 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Oct 20, 2020 at 02:56 PM
+-- Server version: 5.7.30-0ubuntu0.18.04.1
+-- PHP Version: 7.2.24-0ubuntu0.18.04.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `keshbotics`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cron_event`
+--
+
+CREATE TABLE `cron_event` (
+  `event_id` int(11) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
